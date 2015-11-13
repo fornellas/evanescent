ca7bc0e4563e47e89756303ad56caa1050b4f020
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2015-11-12 23:30:57 -0200

    finished rotation, compression and purge

--- lib/evanescent.rb
+++ lib/evanescent.rb
@@ -1,21 +1,120 @@
+require 'chronic_duration'
+
+# IO like object, that can be used with any logging class (such as Ruby's native Logger). This object will save its input to a file, and allows:
+#* Rotation by time / date.
+#* Compression of old files.
+#* Removal of old compressed files.
+# Its purpuse is to supplement logging classes, allowing everything related to logging management, to be done within Ruby, without relying on external tools (such as logrotate).
 class Evanescent
+  # Current path being written to.
+  attr_reader :path
+
+  # Rotation policy.
+  attr_reader :rotation
+
+  # How long rotated files are kept (in seconds).
+  attr_reader :keep
+
+  # Must receive a Hash with:
+  # +:path+:: Path where to write to.
+  # +:rotation+:: Either +:hourly+ or +:daily+.
+  # +:keep+:: For how long to keep rotated files. It is parsed with ChronicDuration's natural language features. Examples: '1 day', '1 month'.
   def initialize opts
-    @read_io, @write_io = IO.pipe
-    @file_io = File.open(opts[:path], 'w+')
-    @thread = Thread.new { sender_thread }
+    @path = opts[:path]
+    @rotation = opts[:rotation]
+    @keep = ChronicDuration.parse(opts[:keep])
+    @mutex = Mutex.new
+    @last_prefix = make_prefix(Time.now)
+    open_file
   end
-  def io
-    @write_io
-  end
-  def close
-    @write_io.close
-    @thread.join
-    @file_io.close
-  end
-  private
-  def sender_thread
-    @read_io.each do |data|
-      @file_io.write(data)
+
+  # Writes to #path and rotate, compress and purge if necessary.
+  def write string
+    @mutex.synchronize do
+      rotate
+      compress
+      purge
+      @io.write(string)
     end
   end
+
+  # Close file.
+  def close
+    @mutex.synchronize do
+      @io.close
+    end
+  end
+
+  private
+
+  def open_file
+    @io = File.open(path, File::APPEND | File::CREAT | File::WRONLY)
+    @io.sync = true
+  end
+
+  PARAMS = {
+    hourly: {
+      strftime: '%Y%m%d%H',
+      glob: '[0-9]' * (4 + 2 * 3)
+    },
+    daily: {
+      strftime: '%Y%m%d',
+      glob: '[0-9]' * (4 + 2 * 2)
+    }
+  }
+
+  def make_prefix time
+    time.strftime(PARAMS[rotation][:strftime])
+  end
+
+  def rotate
+    curr_suffix = make_prefix(Time.now)
+    if curr_suffix != @last_prefix
+      @io.close
+      rotated = "#{path}.#{curr_suffix}"
+      begin
+        FileUtils.mv(path, rotated)
+      rescue
+        warn("Error renaming '#{path}' to '#{rotated}': #{$!}")
+      end
+      open_file
+      @last_prefix = curr_suffix
+    end
+  end
+
+  def compress
+    Dir.glob("#{path}.#{PARAMS[rotation][:glob]}").each do |uncompressed|
+      compressed = "#{uncompressed}.gz"
+      Zlib::GzipWriter.open(compressed) do |gz|
+        gz.mtime = File.mtime(uncompressed)
+        gz.orig_name = uncompressed
+        File.open(uncompressed, 'r') do |io|
+          io.binmode
+          io.each do |data|
+            gz.write(data)
+          end
+        end
+      end
+      File.delete(uncompressed)
+    end
+  rescue
+    warn("Error compressing files: #{$!}")
+  end
+
+  def purge
+    Dir.glob("#{path}.#{PARAMS[rotation][:glob]}.gz").each do |compressed|
+      time_extractor = Regexp.new(
+        '^' + Regexp.escape("#{path}.") + "(?<time>.+)" + Regexp.escape(".gz") + '$'
+      )
+      time_string = compressed.match(time_extractor)[:time]
+      compressed_time = Time.strptime(time_string, PARAMS[rotation][:strftime])
+      age = Time.now - compressed_time
+      if age > keep
+        File.delete(compressed)
+      end
+    end
+  rescue
+    warn("Error purging old files: #{$!}")
+  end
+
 end
--- spec/lib/evanescent_spec.rb
+++ spec/lib/evanescent_spec.rb
@@ -2,25 +2,94 @@ require 'evanescent'
 require 'tmpdir'
 require 'fileutils'
 require 'tempfile'
+require 'timecop'
 
 RSpec.describe Evanescent do
   let(:prefix) { Dir.mktmpdir }
-  let(:log_file) { Tempfile.new('log', prefix).path }
+  let(:path) { Tempfile.new('log', prefix).path }
   after(:example) do
     FileUtils.rm_rf(prefix)
   end
-  let(:data) { 'oeutaohuaorl891237412' }
-  let(:evanescent) do
-    described_class.new(
-      path: log_file,
-    )
-  end
-  it 'writes to file' do
-    times = 3
-    times.times do
-      evanescent.io.write(data)
+  let(:data) do
+    d = []
+    10.times do |t|
+      d << "time\##{t}\n"
     end
-    evanescent.close
-    expect(File.open(log_file).read).to eq(data*times)
+    d
+  end
+  context 'rotation' do
+    def cat path
+      File.open(path, 'r').read
+    end
+    def zcat path
+      Zlib::GzipReader.open(path) do |gz|
+        gz.read
+      end
+    end
+    shared_examples :rotation do
+      it 'rotates and compresses' do
+        Timecop.freeze(start_time)
+        evanescent.write(data_a = data.shift)
+        Timecop.freeze(start_time+interval/2)
+        evanescent.write(data_b = data.shift)
+        first_data = data_a + data_b
+        expect(cat(path)).to eq(first_data)
+
+        Timecop.freeze(start_time + interval)
+        evanescent.write(data_a = data.shift)
+        Timecop.freeze(start_time + interval + interval/2)
+        evanescent.write(data_b = data.shift)
+        second_data = data_a + data_b
+        expect(cat(path)).to eq(second_data)
+        expect(zcat("#{path}.#{sufixes.shift}.gz")).to eq(first_data)
+
+        Timecop.freeze(start_time + interval*2)
+        evanescent.write(data_a = data.shift)
+        Timecop.freeze(start_time + interval*2 + interval/2)
+        evanescent.write(data_b = data.shift)
+        third_data = data_a + data_b
+        expect(cat(path)).to eq(third_data)
+        expect(zcat("#{path}.#{sufixes.shift}.gz")).to eq(second_data)
+
+        files_count = Dir.glob("#{prefix}/*").size
+        expect(files_count).to eq(2)
+      end
+    end
+    context 'hourly' do
+      let(:evanescent) do
+        described_class.new(
+          path: path,
+          rotation: :hourly,
+          keep: '1 hour',
+        )
+      end
+      let(:interval) { 3600 }
+      let(:start_time) { Time.parse('2015-11-12 00:00:00 -0200') }
+      let(:sufixes) do
+        [
+          '2015111201',
+          '2015111202',
+        ]
+      end
+      include_examples :rotation
+    end
+    context 'daily' do
+      let(:evanescent) do
+        described_class.new(
+          path: path,
+          rotation: :daily,
+          keep: '1 day',
+        )
+      end
+      let(:interval) { 3600*24 }
+      let(:start_time) { Time.parse('2015-11-12 00:00:00 -0200') }
+      let(:sufixes) do
+        [
+          '20151113',
+          '20151114',
+        ]
+      end
+      include_examples :rotation
+    end
   end
 end
