67de1e351bbd98061781edee8134a2d904653674
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2015-12-21 19:42:17 -0200

    reorg

--- spec/lib/evanescent_spec.rb
+++ spec/lib/evanescent_spec.rb
@@ -79,69 +79,84 @@ RSpec.describe Evanescent do
         include_examples :old_file_rotation
       end
     end
-    shared_examples :regular_rotation do
-      it 'rotates and compresses' do
-        evanescent.write(data_a = data.shift)
-        Timecop.freeze(start_time+interval/2)
-        evanescent.write(data_b = data.shift)
-        first_data = data_a + data_b
-        evanescent.wait_compression
-        expect(cat(path)).to eq(first_data)
-        Timecop.freeze(start_time + interval)
-        evanescent.write(data_a = data.shift)
-        Timecop.freeze(start_time + interval + interval/2)
-        evanescent.write(data_b = data.shift)
-        second_data = data_a + data_b
-        evanescent.wait_compression
-        expect(cat(path)).to eq(second_data)
-        expect(zcat("#{path}.#{sufixes.shift}.gz")).to eq(first_data)
-        Timecop.freeze(start_time + interval*2)
-        evanescent.write(data_a = data.shift)
-        Timecop.freeze(start_time + interval*2 + interval/2)
-        evanescent.write(data_b = data.shift)
-        third_data = data_a + data_b
-        evanescent.wait_compression
-        expect(cat(path)).to eq(third_data)
-        expect(zcat("#{path}.#{sufixes.shift}.gz")).to eq(second_data)
-        files_count = Dir.glob("#{prefix}/*").size
-        expect(files_count).to eq(2)
+    context 'new file' do
+      shared_examples :regular_rotation do
+        it 'rotates and compresses' do
+          evanescent.write(data_a = data.shift)
+          Timecop.freeze(start_time+interval/2)
+          evanescent.write(data_b = data.shift)
+          first_data = data_a + data_b
+          evanescent.wait_compression
+          expect(cat(path)).to eq(first_data)
+          Timecop.freeze(start_time + interval)
+          evanescent.write(data_a = data.shift)
+          Timecop.freeze(start_time + interval + interval/2)
+          evanescent.write(data_b = data.shift)
+          second_data = data_a + data_b
+          evanescent.wait_compression
+          expect(cat(path)).to eq(second_data)
+          expect(zcat("#{path}.#{sufixes.shift}.gz")).to eq(first_data)
+          Timecop.freeze(start_time + interval*2)
+          evanescent.write(data_a = data.shift)
+          Timecop.freeze(start_time + interval*2 + interval/2)
+          evanescent.write(data_b = data.shift)
+          third_data = data_a + data_b
+          evanescent.wait_compression
+          expect(cat(path)).to eq(third_data)
+          expect(zcat("#{path}.#{sufixes.shift}.gz")).to eq(second_data)
+          files_count = Dir.glob("#{prefix}/*").size
+          expect(files_count).to eq(2)
+        end
       end
-    end
-    context 'hourly' do
-      let(:evanescent) do
-        described_class.new(
+      context 'hourly' do
+        let(:evanescent) do
+          described_class.new(
           path: path,
           rotation: :hourly,
           keep: '1 hour',
-        )
+          )
+        end
+        let(:interval) { 3600 }
+        let(:start_time) { Time.parse("2015-11-03 00:00:00 #{tz}") }
+        let(:sufixes) do
+          [
+            '2015110301',
+            '2015110302',
+          ]
+        end
+        include_examples :regular_rotation
       end
-      let(:interval) { 3600 }
-      let(:start_time) { Time.parse("2015-11-03 00:00:00 #{tz}") }
-      let(:sufixes) do
-        [
-          '2015110301',
-          '2015110302',
-        ]
-      end
-      include_examples :regular_rotation
-    end
-    context 'daily' do
-      let(:evanescent) do
-        described_class.new(
+      context 'daily' do
+        let(:evanescent) do
+          described_class.new(
           path: path,
           rotation: :daily,
           keep: '1 day',
-        )
+          )
+        end
+        let(:interval) { 3600*24 }
+        let(:start_time) { Time.parse("2015-11-03 00:00:00 #{tz}") }
+        let(:sufixes) do
+          [
+            '20151104',
+            '20151105',
+          ]
+        end
+        include_examples :regular_rotation
       end
-      let(:interval) { 3600*24 }
-      let(:start_time) { Time.parse("2015-11-03 00:00:00 #{tz}") }
-      let(:sufixes) do
-        [
-          '20151104',
-          '20151105',
-        ]
+    end
+  end
+  context 'failures' do
+    context '#write' do
+      context '#purge' do
+
       end
-      include_examples :regular_rotation
+      context '#rotate' do
+
+      end
+      context '#compress' do
+
+      end
     end
   end
 end
